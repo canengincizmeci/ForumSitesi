0490cbb5936db068fafaed227259eaa65fd33a84
Can Engin Çizmeci <canncizmeci@gmail.com>
2024-05-26 22:15:25 +0300

    Siirler için ZiyaretciIndex metodu ve sayfası yazıldı

---
+++ adminTabani_01_05_24/Controllers/SiirController.cs
@@ -0,0 +1,29 @@
+﻿using adminTabani_01_05_24.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace adminTabani_01_05_24.Controllers
+{
+    public class SiirController : Controller
+    {
+        // GET: Siir
+        public ActionResult ZiyaretciIndex()
+        {
+            dbContext model = new dbContext();
+            var siirler = model.Siirler.OrderByDescending(p => p.siirID).Where(p => p.onay == true).Select(p => new Siir
+            {
+                siirID=p.siirID,
+                icerik=p.icerik,
+                siirBaslik=p.siirBaslik,
+                tarih=p.tarih,
+                yazar=p.yazar,
+                YazarAd= p.Kullanicilar.Ad,
+                _onay = p.onay
+            }).ToList();
+            return View(siirler);
+        }
+    }
+}
