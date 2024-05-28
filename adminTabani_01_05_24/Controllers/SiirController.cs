7127a54a719fa0124c95ed3113e79fef1ac39c78
Can Engin Çizmeci <canncizmeci@gmail.com>
2024-05-28 21:33:15 +0300

    Ziyaretci İçin şiir detay Ve kullanıcı için Şiir şikayet metodu

--- adminTabani_01_05_24/Controllers/SiirController.cs
+++ adminTabani_01_05_24/Controllers/SiirController.cs
@@ -56,13 +56,13 @@ namespace adminTabani_01_05_24.Controllers
             dbContext model = new dbContext();
             var veriler = model.Siirler.Where(p => p.onay == true).OrderByDescending(p => p.siirID).Select(p => new Siir
             {
-                siirID=p.siirID,
-                icerik=p.icerik,
-                siirBaslik=p.siirBaslik,
-                tarih=p.tarih,
-                yazar=p.yazar,
-                YazarAd=p.Kullanicilar.Ad,
-                _onay=p.onay
+                siirID = p.siirID,
+                icerik = p.icerik,
+                siirBaslik = p.siirBaslik,
+                tarih = p.tarih,
+                yazar = p.yazar,
+                YazarAd = p.Kullanicilar.Ad,
+                _onay = p.onay
             }).ToList();
             return View(veriler);
         }
@@ -83,15 +83,49 @@ namespace adminTabani_01_05_24.Controllers
             }).FirstOrDefault();
             detayPage.siir_yorumlari = model.SiirYorumlar.OrderByDescending(p => p.SiirYorumID).Where(p => p.onay == true).Select(p => new SiirYorumlari
             {
-                _SiirYorumID=p.SiirYorumID,
+                _SiirYorumID = p.SiirYorumID,
                 _onay = p.onay,
-                YorumcuAd=p.Kullanicilar.Ad,
-                _icerik=p.icerik,
-                _siirID=p.siirID,
-                _tarih=p.tarih,
-                _yorumcuID=p.yorumcuID
+                YorumcuAd = p.Kullanicilar.Ad,
+                _icerik = p.icerik,
+                _siirID = p.siirID,
+                _tarih = p.tarih,
+                _yorumcuID = p.yorumcuID
             }).ToList();
             return View(detayPage);
+        }
+        [HttpGet]
+        public ActionResult Bildir(int siir_id)
+        {
+            ViewBag.SiirID = siir_id;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Bildir(int _siirID, string _sebep)
+        {
+            int id = (int)Session["kullanici_id"];
+            dbContext model = new dbContext();
+            model.SiirSikayetler.Add(new SiirSikayetler
+            {
+                sebep = _sebep,
+                siir_id = _siirID,
+                sikayetci_id = id,
+                tarih = DateTime.Now,
+
+            });
+            model.SaveChanges();
+            return RedirectToAction("SiirSikayetTamam", new { siir_id = _siirID });
+        }
+        public ActionResult SiirSikayetTamam(int siir_id)
+        {
+            int id = (int)Session["kullanici_id"];
+            dbContext model = new dbContext();
+            var baslik = model.Siirler.Find(siir_id).siirBaslik;
+            var yazarAd = model.Kullanicilar.Find(id).Ad;
+            ViewBag.Baslik = baslik;
+            ViewBag.Yazar = yazarAd;
+            ViewBag.SiirID = siir_id;
+            return View();
         }
     }
 }
