9d3eee49f583797ca629de942867910bcf71e33b
Can Engin Çizmeci <canncizmeci@gmail.com>
2024-05-28 03:38:33 +0300

    siir için uye ve ziyaretci modunda işlemler yazıldı

--- adminTabani_01_05_24/Controllers/SiirController.cs
+++ adminTabani_01_05_24/Controllers/SiirController.cs
@@ -51,5 +51,47 @@ namespace adminTabani_01_05_24.Controllers
             }).ToList();
             return View(detayPage);
         }
+        public ActionResult UyeSiirIndex()
+        {
+            dbContext model = new dbContext();
+            var veriler = model.Siirler.Where(p => p.onay == true).OrderByDescending(p => p.siirID).Select(p => new Siir
+            {
+                siirID=p.siirID,
+                icerik=p.icerik,
+                siirBaslik=p.siirBaslik,
+                tarih=p.tarih,
+                yazar=p.yazar,
+                YazarAd=p.Kullanicilar.Ad,
+                _onay=p.onay
+            }).ToList();
+            return View(veriler);
+        }
+        public ActionResult UyeSiirDetay(int siir_id)
+        {
+            int id = (int)Session["kullanici_id"];
+            dbContext model = new dbContext();
+            ZiyaretciSiirDetayPage detayPage = new ZiyaretciSiirDetayPage();
+            detayPage.siir = model.Siirler.Where(p => p.siirID == siir_id).Select(p => new Siir
+            {
+                siirID = p.siirID,
+                icerik = p.icerik,
+                siirBaslik = p.siirBaslik,
+                tarih = p.tarih,
+                yazar = p.yazar,
+                YazarAd = p.Kullanicilar.Ad,
+                _onay = p.onay
+            }).FirstOrDefault();
+            detayPage.siir_yorumlari = model.SiirYorumlar.OrderByDescending(p => p.SiirYorumID).Where(p => p.onay == true).Select(p => new SiirYorumlari
+            {
+                _SiirYorumID=p.SiirYorumID,
+                _onay = p.onay,
+                YorumcuAd=p.Kullanicilar.Ad,
+                _icerik=p.icerik,
+                _siirID=p.siirID,
+                _tarih=p.tarih,
+                _yorumcuID=p.yorumcuID
+            }).ToList();
+            return View(detayPage);
+        }
     }
 }
